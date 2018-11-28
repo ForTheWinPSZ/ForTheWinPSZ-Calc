ed7af88a2cad56907d90cac564c82f9ff30b1566
sunhao <aaa1564686692@163.com>
2018-11-28 15:42:33 +0800

    11.28代码改动

---
+++ 04.SourceCode/CalculatorForWin10/CalculatorForWin10.ViewModel/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorForWin10.ViewModel
+{
+    public class MainWindowsViewModel : NotifyObject
+    {
+        public MainWindowsViewModel()
+        {
+            btn_point = new NVCommand(AddBtn_point);
+        }
+
+        private string _resultText;
+        public string ResultText
+        {
+            get { return _resultText; }
+            set { SetPropertyNotify(ref _resultText, value, nameof(ResultText)); }
+        }
+
+       
+
+       
+        private readonly NVCommand btn_point;
+        public NVCommand Btn_point { get => btn_point; }
+
+
+        private void AddBtn_point() { ResultText = "SS"; }
+       
+
+    }
+}
