6f01ecdf8c2377fd35fe04369a182fc67c5301f6
sunhao <aaa1564686692@163.com>
2018-11-29 16:42:46 +0800

    S

---
+++ 04.SourceCode/CalculatorForWin10/CalculatorForWin10.ViewModel/Screen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorForWin10.ViewModel
+{
+    class Screen
+    {
+        private string resultValue;
+        private string expressionValue;
+
+        private static Screen screen = new Screen();
+
+        public string ResultValue { get => resultValue; set => resultValue = value; }
+        public string ExpressionValue { get => expressionValue; set => expressionValue = value; }
+
+        private Screen() { }
+
+        public static Screen GetScreen() {
+            return screen;
+        }
+    }
+}
--- 04.SourceCode/CalculatorForWin10/CalculatorForWin10.ViewModel/ViewModel.cs
+++ 04.SourceCode/CalculatorForWin10/CalculatorForWin10.ViewModel/ViewModel.cs
@@ -9,9 +9,23 @@ namespace CalculatorForWin10.ViewModel
 {
     public class MainWindowsViewModel : NotifyObject
     {
+        Screen screen = Screen.GetScreen();
+
+
         public MainWindowsViewModel()
         {
             btn_point = new NVCommand(AddBtn_point);
+            btn_0 = new NVCommand(AddBtn_0);
+            btn_1 = new NVCommand(AddBtn_1);
+            btn_2 = new NVCommand(AddBtn_2);
+            btn_3 = new NVCommand(AddBtn_3);
+            btn_4 = new NVCommand(AddBtn_4);
+            btn_5 = new NVCommand(AddBtn_5);
+            btn_6 = new NVCommand(AddBtn_6);
+            btn_7 = new NVCommand(AddBtn_7);
+            btn_8 = new NVCommand(AddBtn_8);
+            btn_9 = new NVCommand(AddBtn_9);
+
         }
 
         private string _resultText;
@@ -24,12 +38,89 @@ namespace CalculatorForWin10.ViewModel
        
 
        
-        private readonly NVCommand btn_point;
+        
         public NVCommand Btn_point { get => btn_point; }
 
+        public NVCommand Btn_01 => btn_0;
 
-        private void AddBtn_point() { ResultText = "SS"; }
-       
+        public NVCommand Btn_1 => btn_1;
+
+        public NVCommand Btn_2 => btn_2;
+
+        public NVCommand Btn_3 => btn_3;
+
+        public NVCommand Btn_4 => btn_4;
+
+        public NVCommand Btn_5 => btn_5;
+
+        public NVCommand Btn_6 => btn_6;
+
+        public NVCommand Btn_7 => btn_7;
+
+        public NVCommand Btn_8 => btn_8;
+
+        public NVCommand Btn_9 => btn_9;
+
+        private readonly NVCommand btn_point;
+        private readonly NVCommand btn_0;
+        private readonly NVCommand btn_1;
+        private readonly NVCommand btn_2;
+        private readonly NVCommand btn_3;
+        private readonly NVCommand btn_4;
+        private readonly NVCommand btn_5;
+        private readonly NVCommand btn_6;
+        private readonly NVCommand btn_7;
+        private readonly NVCommand btn_8;
+        private readonly NVCommand btn_9;
+
+
+        private void AddBtn_point() {
+            new Number().InputText(".");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_1() {
+            new Number().InputText("1");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_2() {
+            new Number().InputText("2");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_3() {
+            new Number().InputText("3");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_4() {
+            new Number().InputText("4");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_5() {
+            new Number().InputText("5");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_6() {
+            new Number().InputText("6");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_7() {
+            new Number().InputText("7");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_8() {
+            new Number().InputText("8");
+            ResultText += screen.ResultValue;
+        }
+        private void AddBtn_9() {
+            new Number().InputText("9");
+            ResultText += screen.ResultValue;
+        }
+
+        private void AddBtn_0()
+        {
+            new Number().InputText("0");
+            ResultText += screen.ResultValue;
+        }
+
 
     }
 }
