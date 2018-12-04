5d06387aacca17f0c92be40acd35bc02059eda80
sunhao <aaa1564686692@163.com>
2018-12-04 13:44:28 +0800

    S

--- 04.SourceCode/CalculatorForWin10/Arithmetic/BinaryOperaion/Multiplication.cs
+++ 04.SourceCode/CalculatorForWin10/Arithmetic/BinaryOperaion/Multiplication.cs
@@ -11,8 +11,6 @@ namespace Arithmetic.BinaryOperation
 {
     public class Multiplication : IBinary
     {
-        //用来存储一开始resultValue的值
-        string _preResultValue;
         public Multiplication(string resultValue, string expressionValue, string preResult)
         {
             base.ResultValue = resultValue;
@@ -25,7 +23,7 @@ namespace Arithmetic.BinaryOperation
             {
                 ResultValue = ResultValue.Substring(0, ResultValue.Length - 1);
             }
-            _preResultValue = ResultValue;
+           
             ChangeResultValue();
             ChangeExpression();
         }
@@ -35,7 +33,7 @@ namespace Arithmetic.BinaryOperation
             //表达式没有值的时候
             if (ExpressionValue == "")
             { 
-                ExpressionValue= _preResultValue + " × ";
+                ExpressionValue= ResultValue + " × ";
                 return;
             }
             else
@@ -55,20 +53,26 @@ namespace Arithmetic.BinaryOperation
                     return;
                 }
                 //表达式有值且是数字定义的时候
-                ExpressionValue= ExpressionValue + _preResultValue + " × ";
+                ExpressionValue= ExpressionValue + ResultValue + " × ";
                 return;
             }
         }
 
         public override void ChangeResultValue()
         {
-            Debug.WriteLine("IsComplete:"+IsComplete);
-            Debug.WriteLine("暂存值：" + PreResult);
-            //不能计算
+            
+            //初始时候不能计算
             if (ResultValue == "" && PreResult == "")
             {
                 return;
             }
+            //表达式最后为加减乘除
+            if(ExpressionValue.EndsWith(" ")&&ResultValue=="")
+            {
+                return;
+            }
+            
+ 
             //能计算的情况
             if (IsComplete)                         
             {                
@@ -103,15 +107,7 @@ namespace Arithmetic.BinaryOperation
                         else
                         {
                             exps = exps.Substring(0, index + 1)+PreResult;
-                            exps=exps.Replace(" ", "");
-                            if (exps.Contains("×"))
-                            {
-                                exps = exps.Replace('×', '*');
-                            }
-                            else if (exps.Contains("÷"))
-                            {
-                                exps = exps.Replace('÷', '/');
-                            }
+                            exps = ForCompute(exps);
                             PreResult = table.Compute(exps, "").ToString();
                             return;
                         }
@@ -130,12 +126,27 @@ namespace Arithmetic.BinaryOperation
                     string cul = PreResult + symbol + ResultValue;
                     cul = cul.Replace(" ", "");
                     PreResult = table.Compute(cul, "").ToString();
-                    ResultValue= PreResult;
                     return;
                 }
             }
 
         }
+
+        //去掉空格，将X和÷替换为可以计算的
+        private static string ForCompute(string expression)
+        {
+            expression = expression.Trim().Replace(" ","");
+            if (expression.Contains("×"))
+            {
+                expression = expression.Replace('×', '*');
+            }
+            else if (expression.Contains("÷"))
+            {
+                expression = expression.Replace('÷', '/');
+            }
+            return expression;
+        }
+
 
         public override string ReturnExpressionValue()
         {
