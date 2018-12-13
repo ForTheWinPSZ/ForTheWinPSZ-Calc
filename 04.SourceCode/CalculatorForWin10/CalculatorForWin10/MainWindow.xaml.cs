b1bdc6c7ee100850471b5e20d26810daa8d0ed39
panming1108 <panming1108@163.com>
2018-12-13 12:30:17 +0800

    新界面

--- 04.SourceCode/CalculatorForWin10/CalculatorForWin10/MainWindow.xaml.cs
+++ 04.SourceCode/CalculatorForWin10/CalculatorForWin10/MainWindow.xaml.cs
@@ -8,109 +8,119 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Input;
+using MahApps.Metro.Controls;
 namespace CalculatorForWin10
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-        bool _clicked = false;
-
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private bool IsHistoryOpened = false;
+        private bool IsMemoryOpened = false;
+        private bool Arrived = false;
+        private Grid extension;
+        private UserControl extensionControl;
         public MainWindow()
         {
+            extensionControl = new Extension();
+            extension = new Grid();
+            extension.Width = 320;
+            extension.SetValue(Grid.ColumnProperty, 1);
+            extension.SetValue(NameProperty, "Second");
+            extension.Children.Add(extensionControl);
+
             InitializeComponent();
-            this.DataContext = new MainWindowsViewModel();
-        }
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            IconHelper.RemoveIcon(this);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void MetroWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (_clicked)
+            this.DragMove();
+        }
+
+        private void Btn_Min_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Btn_Max_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
             {
-                main.Width = 360;
-                smallClose.Visibility = Visibility.Visible;
-                smallMin.Visibility = Visibility.Visible;
-                smallMax.Visibility = Visibility.Visible;
-                bigClose.Visibility = Visibility.Hidden;
-                bigMin.Visibility = Visibility.Hidden;
-                bigMax.Visibility = Visibility.Hidden;
-                _clicked = false;
+                WindowState = WindowState.Normal;
             }
             else
             {
-                main.Width = 700;
-                bigClose.Visibility = Visibility.Visible;
-                bigMin.Visibility = Visibility.Visible;
-                bigMax.Visibility = Visibility.Visible;
-                smallClose.Visibility = Visibility.Hidden;
-                smallMin.Visibility = Visibility.Hidden;
-                smallMax.Visibility = Visibility.Hidden;
-                _clicked = true;
+                WindowState = WindowState.Maximized;
             }
-            
         }
 
-        private void Button_Click_Close(object sender, RoutedEventArgs e)
+        private void Btn_Close_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
-        private void Button_Click_Max(object sender, RoutedEventArgs e)
+        private void Btn_History_Click(object sender, RoutedEventArgs e)
         {
-            if (this.WindowState == WindowState.Maximized)
+            this.HistoryFlyout.Visibility = Visibility.Visible;
+            if (IsHistoryOpened)
             {
-                if (_clicked)
-                {
-                    bigClose.Visibility = Visibility.Visible;
-                    bigMin.Visibility = Visibility.Visible;
-                    bigMax.Visibility = Visibility.Visible;
-                    smallClose.Visibility = Visibility.Hidden;
-                    smallMin.Visibility = Visibility.Hidden;
-                    smallMax.Visibility = Visibility.Hidden;
-                }
-                else
-                {
-                    smallClose.Visibility = Visibility.Visible;
-                    smallMin.Visibility = Visibility.Visible;
-                    smallMax.Visibility = Visibility.Visible;
-                    bigClose.Visibility = Visibility.Hidden;
-                    bigMin.Visibility = Visibility.Hidden;
-                    bigMax.Visibility = Visibility.Hidden;
-                }
-                this.WindowState = WindowState.Normal; //设置窗口还原
+                this.HistoryFlyout.IsOpen = false;
+                IsHistoryOpened = false;
             }
             else
             {
-                bigClose.Visibility = Visibility.Visible;
-                bigMin.Visibility = Visibility.Visible;
-                bigMax.Visibility = Visibility.Visible;
-                smallClose.Visibility = Visibility.Hidden;
-                smallMin.Visibility = Visibility.Hidden;
-                smallMax.Visibility = Visibility.Hidden;
-                this.WindowState = WindowState.Maximized; //设置窗口最大化
+                this.HistoryFlyout.IsOpen = true;
+                IsHistoryOpened = true;
             }
         }
 
-        private void Button_Click_Min(object sender, RoutedEventArgs e)
+        private void Btn_Memory_Click(object sender, RoutedEventArgs e)
         {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void Grid_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            this.MemoryFlyout.Visibility = Visibility.Visible;
+            if (IsMemoryOpened)
             {
-                this.DragMove();
+                this.MemoryFlyout.IsOpen = false;
+                IsMemoryOpened = false;
+            }
+            else
+            {
+                this.MemoryFlyout.IsOpen = true;
+                IsMemoryOpened = true;
             }
         }
+
+        private void MetroWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.Width > 624 && Arrived == false)
+            {
+                this.MaxWidth = SystemParameters.WorkArea.Width;
+                ColumnDefinition col2 = new ColumnDefinition();
+                col2.MinWidth = 320;
+                col2.MaxWidth = 320;
+                Main.ColumnDefinitions.Add(col2);
+                Main.Children.Add(extension);
+                btn_history.Visibility = Visibility.Hidden;
+                btn_mopt.Visibility = Visibility.Hidden;
+                Arrived = true;
+            }
+
+            if (this.Width <= 624 && Arrived == true)
+            {
+                extension = Main.FindChild<Grid>("Second");
+                Main.Children.Remove(extension);
+                Main.ColumnDefinitions.RemoveAt(1);
+                btn_history.Visibility = Visibility.Visible;
+                btn_mopt.Visibility = Visibility.Visible;
+                Arrived = false;
+            }
+        }
+
+
     }
 }
