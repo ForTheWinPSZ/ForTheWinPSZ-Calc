f5570aa2813d0a30265e2a0b591097b03d8ec935
panming1108 <45291616+panming1108@users.noreply.github.com>
2018-12-19 09:14:33 +0800

    Revert "12/19gittest"

--- 04.SourceCode/CalculatorForWin10/CalculatorForWin10/History.xaml.cs
+++ 04.SourceCode/CalculatorForWin10/CalculatorForWin10/History.xaml.cs
@@ -81,7 +81,6 @@ namespace CalculatorForWin10
 
         private void Button_Click_Qc(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("test");
             H.Visibility = Visibility.Visible;
             btn_history_dustbin.Visibility = Visibility.Hidden;
         }
