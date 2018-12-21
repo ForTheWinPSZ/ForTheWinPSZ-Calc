eb8369583567977e62ff1e923a27834f0d330849
sunhao <aaa1564686692@163.com>
2018-12-21 10:40:21 +0800

    1

--- 04.SourceCode/CalculatorForWin10/Arithmetic/BinaryOperaion/Plus.cs
+++
@@ -1,10 +0,0 @@
-﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using System.Text.RegularExpressions;using System.Data;using System.Diagnostics;using Arithmetic.BinaryOperaion;namespace Arithmetic.BinaryOperation{
-    public class Plus : IBinary    {
-        public Plus(string resultValue, string expressionValue, string preResult):base(resultValue, expressionValue, preResult)        {        }
-        
-        public override void Change()        {            if (ExpressionValue == "")            {                PreResult = (ResultValue == "" ? PreResult : ResultValue);                ExpressionValue = HistoryDisplayTool.MaxContain(PreResult) + " + ";            }            else            {                if (!ExpressionValue.EndsWith(" "))                {                    string[] arr = ExpressionValue.Split(new char[] { ' ' });                    if (arr.Length < 3)                        PreResult = ResultValue;                    else                        PreResult = Tool.Compute(PreResult, arr[arr.Length - 2], ResultValue);
-                    ExpressionValue = ExpressionValue + " + ";
-                }                else if (ResultValue == "") //换符号                {                    ExpressionValue = ExpressionValue.Substring(0, ExpressionValue.Length - 3) + " + ";
-                }                else                {                    string symbol = ExpressionValue.Substring(ExpressionValue.Length - 3).Trim();                    ExpressionValue = ExpressionValue + HistoryDisplayTool.MaxContain(ResultValue) + " + ";                    PreResult = Tool.Compute(PreResult, symbol, ResultValue);
-                }            }
-        }    }}
--- 04.SourceCode/CalculatorForWin10/Arithmetic/Equal.cs
+++
@@ -1,52 +0,0 @@
-﻿using Arithmetic.BinaryOperaion;using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using System.Diagnostics;namespace Arithmetic{    public class Equal    {        private string resultValue;        private string expressionValue;        private string preResult;        private List<History> history;        private string historyString = "";        private static string lparm;        private string _lhistory;
-        private string lastHistory;        char[] strs = { '不', '无', '未', '溢' };        public Equal(string resultValue, string expressionValue, string preResult, List<History> history, string lparm, string lhistory)        {            this.resultValue = resultValue;            this.expressionValue = expressionValue;            this.preResult = preResult;            this.history = history;            if (preResult.IndexOfAny(strs)!=-1)
-            {
-                this.preResult = "0";
-                this.expressionValue = "";
-                return;
-            }            _lhistory = lhistory;          
-            CaculationResult();
-            AddHistory();                               }        public void CaculationResult()        {
-                        if (expressionValue != "")            {                if (!expressionValue.EndsWith(" "))                {                                       if (expressionValue.Split(new char[] { ' ' }).Length==1 && LastHistoryHasBinary())
-                    {
-                        string[] arr1 = lastHistory.Split(new char[] { ' ' });
-                        string sym = arr1[arr1.Length - 4].Trim();
-                        preResult= Tool.Compute(resultValue,sym, lparm);
-                        historyString = expressionValue + " " + sym + " "+HistoryDisplayTool.MaxContain(lparm);
-                    }
-                    else
-                    {
-                        lparm = resultValue == "" ? preResult : resultValue;
-                        historyString = expressionValue;
-                        string[] arr = expressionValue.Split(new char[] { ' ' });
-                        if (arr.Length < 3)
-                            preResult = resultValue;
-                        else
-                            preResult = Tool.Compute(preResult, arr[arr.Length - 2], resultValue);
-                    }                                  }                else                {                    if (resultValue.Contains("."))
-                        resultValue = Convert.ToDecimal(resultValue).ToString();                    string symbol = expressionValue.Substring(expressionValue.Length - 3).Trim();                    if ((resultValue == "" && preResult == "0" && symbol == "÷") || (resultValue == "0" && preResult == "0" && symbol == "÷"))
-                        preResult = "结果未定义";
-                    else
-                    {
-                        lparm = resultValue == "" ? preResult : resultValue;
-                        historyString = expressionValue + (resultValue == "" ? HistoryDisplayTool.MaxContain(preResult) : HistoryDisplayTool.MaxContain(resultValue));
-                        preResult = Tool.Compute(preResult, symbol, resultValue == "" ? preResult : resultValue);
-
-                    }
-                }            }            else            {                                if (preResult != "")                {                    if (LastHistoryHasBinary())                    {                        string[] arr = lastHistory.Split(new char[] { ' ' });                                                                      string symbol = arr[arr.Length - 4].Trim();                        string lastParam = arr[arr.Length - 3].Trim();
-                        if (lastParam.EndsWith(")"))
-                            lastParam = lparm;                                        historyString = (resultValue == "" ? HistoryDisplayTool.MaxContain(preResult): HistoryDisplayTool.MaxContain(resultValue)) + " " + symbol + " " + HistoryDisplayTool.MaxContain(lastParam);                        preResult =Tool.Compute(resultValue == "" ? preResult : resultValue, symbol, lastParam);                    }                    else                    {                        historyString = resultValue == "" ? HistoryDisplayTool.MaxContain(preResult) : HistoryDisplayTool.MaxContain(resultValue);                        preResult = historyString;                    }                }                else                {                    historyString = resultValue;                    preResult = historyString;                }            }        }        public void AddHistory()        {            if (preResult.IndexOfAny(strs) == -1)
-            {
-                History his = new History(historyString + " = ",HistoryDisplayTool.AddComma(HistoryDisplayTool.MaxContain(preResult)));
-                history.Add(his);
-            }        }        public string ReturnExpressionValue()        {                        if(preResult.IndexOfAny(strs) != -1)
-            {
-                return expressionValue;
-            }            return "";        }        public string ReturnPreResult()        {            return preResult;        }        public string Lparm()        {            return lparm;        }        public List<History> ReturnHistory()        {            return history;        }        public bool LastHistoryHasBinary()        {
-            if (history.Count == 0)            {                lastHistory = _lhistory;
-            }
-            else
-            {
-                lastHistory = history.Last<History>().ToString();
-            }            ;
-            if (lastHistory.Split(new char[] { ' ' }).Length>=5)            {                return true;            }            return false;        }    }}
